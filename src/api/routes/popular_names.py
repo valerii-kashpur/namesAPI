bb4b593425c3f864c1a4a5103003660ae1efa25b
Valerii Kashpur <67324553+valerii-kashpur@users.noreply.github.com>
2025-05-27 17:17:50 +0300

    feat/auth req + deps update

--- src/api/routes/popular_names.py
+++ src/api/routes/popular_names.py
@@ -1,9 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Security
 
-from src.api.routes.names import get_name_service
 from src.api.schemas import PopularNameResponse
+from src.dependencies.names_deps import get_name_service
+from src.dependencies.user_deps import get_current_user
 from src.exceptions.custom_exceptions import APIError, DatabaseError
 from src.services.name_service import NameService
 
@@ -11,7 +12,11 @@ router = APIRouter(tags=["popular_names"])
 
 
 @router.get("/popular-names/", response_model=List[PopularNameResponse])
-async def get_popular_names_by_country(country: str, service: NameService = Depends(get_name_service)):
+async def get_popular_names_by_country(
+        country: str,
+        service: NameService = Depends(get_name_service),
+        current_user=Security(get_current_user)
+):
     if not country:
         raise HTTPException(status_code=400, detail="Country parameter is required")
     try:
