dec88b2cd3740ab0d281dd1aab176209779e0acb
Valerii Kashpur <67324553+valerii-kashpur@users.noreply.github.com>
2025-05-27 17:14:15 +0300

    feat/auth schemas

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -85,3 +85,19 @@ class PopularNameResponse(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
