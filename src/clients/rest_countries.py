a360af805ac8cdcfb614763b74fad69dbb5f95be
Valerii Kashpur <67324553+valerii-kashpur@users.noreply.github.com>
2025-05-27 14:20:10 +0300

    fix/not list expected

--- src/clients/rest_countries.py
+++ src/clients/rest_countries.py
@@ -14,6 +14,6 @@ class RestCountriesClient(RestCountriesClientInterface):
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://restcountries.com/v3.1/alpha/{code}") as response:
                 if response.status != 200:
-                    raise ValueError("Failed to fetch data from REST Countries")
+                    return None
                 data = await response.json()
-                return data[0] if data else None
+                return data
