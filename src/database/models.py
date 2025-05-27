7577a436d9c0c5a4f320d193aa3a30a04fdb1ebd
Valerii Kashpur <67324553+valerii-kashpur@users.noreply.github.com>
2025-05-27 17:12:40 +0300

    feat/user model

--- src/database/models.py
+++ src/database/models.py
@@ -46,3 +46,11 @@ class NameCountry(Base):
     probability = Column(Float)
     name = relationship("Name", back_populates="countries")
     country = relationship("Country", back_populates="names")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
