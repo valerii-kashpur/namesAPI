27eca0f4463028b54d6e92d3491bd9aca2b7d2f1
Valerii Kashpur <67324553+valerii-kashpur@users.noreply.github.com>
2025-05-28 14:17:14 +0300

    fix/exception fix

--- src/exceptions/custom_exceptions.py
+++ src/exceptions/custom_exceptions.py
@@ -8,10 +8,11 @@ class AppException(Exception):
         super().__init__(self.message)
 
 
-class APIError(AppException):
-    def __init__(self, message: str, details: Optional[str] = None, status_code: int = 502):
-        self.details = details
-        super().__init__(message, status_code)
+class APIError(Exception):
+    def __init__(self, message: str, status_code: int = 502):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(self.message)
 
 
 class DatabaseError(AppException):
